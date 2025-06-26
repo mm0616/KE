9c3b50c081a5ff55508da08777a25410ab361844
momo <1668817352@qq.com>
2025-06-26 17:45:18 +0800

    整理文档

--- conf.py
+++ conf.py
@@ -17,19 +17,16 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'keyes WiKi'
-author = 'keyes'
-copyright = 'Shenzhen keyes diy robot Co., Ltd'
-
-root_doc = "index"
-master_doc = "index"
+project = 'keyestudio WiKi'
+copyright = 'keyestudio , WiKi'
+author = 'keyestudio WiKi'
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_copybutton']
+extensions = ["sphinx_copybutton"]
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -46,10 +43,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
-html_theme_options = {
-  "show_toc_level": 3,
-}
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
