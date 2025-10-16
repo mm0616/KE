8c500295a54908ecfcde25f462712a9535f873b0
momo <1668817352@qq.com>
2025-10-16 17:28:53 +0800

    整理文档

--- conf.py
+++ conf.py
@@ -17,16 +17,20 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Mini Tank Robot V2'
-copyright = 'keyestudio wiki'
-author = 'keyestudio'
+project = 'keyes WiKi'
+copyright = 'keyes , WiKi'
+author = 'keyes WiKi'
+copyright = 'Shenzhen keyes Technology Co., Ltd'
+
+root_doc = "index"
+master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser','sphinx_copybutton']
+extensions = ["myst_parser","sphinx_copybutton"]
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -64,7 +68,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+  "show_toc_level": 3,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- index.rst
+++ index.rst
@@ -4,11 +4,8 @@
    :caption: Contents:
 
 
-   docs/1. Product introduction/a
-   docs/2. Product installation/a
-   docs/3. Arduino/3. Arduino.rst
-   docs/4. Scratch/4. Scratch.rst
-   docs/5. Driver installation/a
+   docs/Mixly/2.rst
+
 
 
 
